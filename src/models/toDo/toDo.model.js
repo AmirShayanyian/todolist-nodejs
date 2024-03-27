d8c00f0fb0be271a95b0103147ea0ba68a025960
amirshayanyian <wahtamir12@gmail.com>
2024-03-28 01:02:46 +0330

    created a to-do model using a Schema

---
+++ src/models/toDo/toDo.model.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+
+const toDoSchema = new Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    description: {
+      type: String,
+      max: [250, 'To-Do description cannot be more than 250 characters !'],
+    },
+    isDone: { type: Boolean, default: false, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const toDoModel = model('todo', toDoSchema);
+
+module.exports = toDoModel;
